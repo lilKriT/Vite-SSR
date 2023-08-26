42e15c1e6e58e5d3294090a05a32588f7c1155da
Piotr Filipiak <kiddhkane@gmail.com>
2023-08-26 21:55:02 +0200

    Added routes.

--- server/controllers/TaskController.ts
+++ server/controllers/TaskController.ts
@@ -5,4 +5,20 @@ const getAllTasks = expressAsyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ msg: "Tasks!" });
 });
 
-export { getAllTasks };
+const getTask = expressAsyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ msg: "Single task" });
+});
+
+const createTask = expressAsyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ msg: "Creating task" });
+});
+
+const updateTask = expressAsyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ msg: "Updating task" });
+});
+
+const deleteTask = expressAsyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ msg: "Deleting task" });
+});
+
+export { getAllTasks, getTask, createTask, updateTask, deleteTask };
--- server/routes/TaskRoutes.ts
+++ server/routes/TaskRoutes.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { getAllTasks } from "../controllers/TaskController.js";
+import {
+  createTask,
+  deleteTask,
+  getAllTasks,
+  getTask,
+  updateTask,
+} from "../controllers/TaskController.js";
 
 const taskRoutes = Router();
 
 taskRoutes.get("/", getAllTasks);
+taskRoutes.get("/:id", getTask);
+taskRoutes.post("/", createTask);
+taskRoutes.put("/:id", updateTask);
+taskRoutes.delete("/:id", deleteTask);
 
 export default taskRoutes;
