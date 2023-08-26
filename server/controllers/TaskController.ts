9353debb94e2d5b8c67078824a0e7b749cf95a53
Piotr Filipiak <kiddhkane@gmail.com>
2023-08-26 22:19:58 +0200

    You can remove and update tasks.

--- server/controllers/TaskController.ts
+++ server/controllers/TaskController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import expressAsyncHandler from "express-async-handler";
 import Task from "../models/Task.js";
+import taskRoutes from "../routes/TaskRoutes.js";
 
 const getAllTasks = expressAsyncHandler(async (req: Request, res: Response) => {
   const allTasks = await Task.find();
@@ -8,7 +9,14 @@ const getAllTasks = expressAsyncHandler(async (req: Request, res: Response) => {
 });
 
 const getTask = expressAsyncHandler(async (req: Request, res: Response) => {
-  res.status(200).json({ msg: "Single task" });
+  const { id } = req.params;
+  const task = await Task.findById(id);
+
+  if (!task) {
+    res.status(400);
+    throw new Error("Task not found.");
+  }
+  res.status(200).json(task);
 });
 
 const createTask = expressAsyncHandler(async (req: Request, res: Response) => {
@@ -24,11 +32,33 @@ const createTask = expressAsyncHandler(async (req: Request, res: Response) => {
 });
 
 const updateTask = expressAsyncHandler(async (req: Request, res: Response) => {
-  res.status(200).json({ msg: "Updating task" });
+  const { id } = req.params;
+  const updatedArgs = { ...req.body };
+
+  const updatedTask = await Task.findByIdAndUpdate(id, updatedArgs, {
+    new: true,
+  });
+
+  if (!updatedTask) {
+    res.status(400);
+    throw new Error("Task couldn't be updated.");
+  }
+
+  res.status(200).json(updatedTask);
 });
 
 const deleteTask = expressAsyncHandler(async (req: Request, res: Response) => {
-  res.status(200).json({ msg: "Deleting task" });
+  const { id } = req.params;
+
+  const taskToDelete = await Task.findById(id);
+  if (!taskToDelete) {
+    res.status(400);
+    throw new Error("Task not found.");
+  }
+
+  await taskToDelete.deleteOne();
+
+  res.status(200).json({ msg: `Task with id: ${id} deleted.` });
 });
 
 export { getAllTasks, getTask, createTask, updateTask, deleteTask };
